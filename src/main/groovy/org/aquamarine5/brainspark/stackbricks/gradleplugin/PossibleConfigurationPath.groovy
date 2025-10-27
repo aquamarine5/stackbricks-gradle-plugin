c483ddaf4c61976c75a9c948149ba10356ff63a9
aquamarine5 <3168287806@QQ.com>
2025-10-27 10:00:29 +0800

    m24

--- src/main/groovy/org/aquamarine5/brainspark/stackbricks/gradleplugin/PossibleConfigurationPath.groovy
+++ src/main/groovy/org/aquamarine5/brainspark/stackbricks/gradleplugin/PossibleConfigurationPath.groovy
@@ -4,4 +4,10 @@ class PossibleConfigurationPath {
     String host;
     String configFilePath;
     Boolean isHttps = false;
+
+    PossibleConfigurationPath(String host,String configFilePath,Boolean isHttps=false){
+        this.host=host
+        this.configFilePath=configFilePath
+        this.isHttps=isHttps
+    }
 }
